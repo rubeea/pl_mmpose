8e9f126a34d1dc2e8d07d9fa64e3627d7c0bff8a
rubeea <rubeea@users.noreply.github.com>
2021-05-09 08:02:21 +0800

    Update __init__.py
    Added set_random_seed() method

--- mmpose/apis/__init__.py
+++ mmpose/apis/__init__.py
@@ -3,10 +3,10 @@ from .inference import (inference_bottom_up_pose_model,
                         vis_pose_result)
 from .inference_tracking import get_track_id, vis_pose_tracking_result
 from .test import multi_gpu_test, single_gpu_test
-from .train import train_model
+from .train import train_model,set_random_seed
 
 __all__ = [
-    'train_model', 'init_pose_model', 'inference_top_down_pose_model',
+    'set_random_seed','train_model', 'init_pose_model', 'inference_top_down_pose_model',
     'inference_bottom_up_pose_model', 'multi_gpu_test', 'single_gpu_test',
     'vis_pose_result', 'get_track_id', 'vis_pose_tracking_result'
 ]
