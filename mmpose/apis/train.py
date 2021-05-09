766eb404849dd6b68a98b82b65d52536c75fa1bb
rubeea <rubeea@users.noreply.github.com>
2021-05-09 08:01:41 +0800

    Update train.py
    Added set_random_seed() method

--- mmpose/apis/train.py
+++ mmpose/apis/train.py
@@ -1,4 +1,7 @@
+import random
 import warnings
+
+import numpy as np
 
 import mmcv
 import torch
@@ -20,7 +23,24 @@ else:
         'Please upgrade your mmcv version, if needed.', DeprecationWarning)
     from mmpose.core import Fp16OptimizerHook
 
-
+def set_random_seed(seed, deterministic=False):
+    """Set random seed.
+    Args:
+        seed (int): Seed to be used.
+        deterministic (bool): Whether to set the deterministic option for
+            CUDNN backend, i.e., set `torch.backends.cudnn.deterministic`
+            to True and `torch.backends.cudnn.benchmark` to False.
+            Default: False.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    
+    
 def train_model(model,
                 dataset,
                 cfg,
