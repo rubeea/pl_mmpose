93136b539370c40b4affbe273c5c95045d1b6643
rubeea <rubeeajaff@gmail.com>
2021-05-19 10:22:27 +0800

    updated dataset init.py to include MendeleyPLDataset

--- mmpose/datasets/__init__.py
+++ mmpose/datasets/__init__.py
@@ -14,7 +14,7 @@ from .datasets import (  # isort:skip
     TopDownCrowdPoseDataset, TopDownFreiHandDataset, TopDownJhmdbDataset,
     TopDownMhpDataset, TopDownMpiiDataset, TopDownMpiiTrbDataset,
     TopDownOCHumanDataset, TopDownOneHand10KDataset, TopDownPanopticDataset,
-    TopDownPoseTrack18Dataset)
+    TopDownPoseTrack18Dataset, MendeleyPLDataset)
 
 __all__ = [
     'AnimalATRWDataset', 'TopDownCocoDataset', 'BottomUpCocoDataset',
@@ -28,7 +28,7 @@ __all__ = [
     'TopDownPanopticDataset', 'TopDownPoseTrack18Dataset',
     'TopDownJhmdbDataset', 'TopDownMhpDataset', 'Face300WDataset',
     'AnimalHorse10Dataset', 'AnimalMacaqueDataset', 'AnimalFlyDataset',
-    'AnimalLocustDataset', 'AnimalZebraDataset', 'AnimalPoseDataset',
+    'AnimalLocustDataset', 'AnimalZebraDataset', 'AnimalPoseDataset', 'MendeleyPLDataset',
     'build_dataloader', 'build_dataset', 'Compose', 'DistributedSampler',
     'DATASETS', 'PIPELINES'
 ]
