81a22d2c028423a3220e8a9fe7a16ef86fec5a79
rubeea <rubeeajaff@gmail.com>
2021-06-24 12:54:13 +0800

    Update train.py
    numpy packpage added

--- mmpose/apis/train.py
+++ mmpose/apis/train.py
@@ -1,6 +1,6 @@
 import random
 import warnings
-
+import numpy as np
 import torch
 from mmcv.parallel import MMDataParallel, MMDistributedDataParallel
 from mmcv.runner import DistSamplerSeedHook, EpochBasedRunner, OptimizerHook
