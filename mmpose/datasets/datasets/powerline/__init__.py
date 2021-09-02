8ae8c77241ae35c182dacdae4b9a24088192d0c7
rubeea <rubeeajaff@gmail.com>
2021-09-02 13:36:14 +0800

    pldm_keypoints_3_files added

--- mmpose/datasets/datasets/powerline/__init__.py
+++ mmpose/datasets/datasets/powerline/__init__.py
@@ -1,6 +1,7 @@
 from .mendeley_dataset import MendeleyPLDataset
 from .mendeley_bottom_up import MendeleyBottomUpDataset
 from .pldu_dataset import PLDUDataset
+from .pldm_dataset import PLDMDataset
 
-__all__ = ['MendeleyPLDataset', 'MendeleyBottomUpDataset', 'PLDUDataset']
+__all__ = ['MendeleyPLDataset', 'MendeleyBottomUpDataset', 'PLDUDataset', 'PLDMDataset']
 
