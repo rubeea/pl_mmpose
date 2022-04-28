760691deeec8d7b9a9d8bdbcdcac9f01a5518b44
rubeea <rubeeajaff@gmail.com>
2022-04-28 13:12:56 +0800

    Mooring Dataset added

--- mmpose/datasets/datasets/powerline/__init__.py
+++ mmpose/datasets/datasets/powerline/__init__.py
@@ -2,6 +2,7 @@ from .mendeley_dataset import MendeleyPLDataset
 from .mendeley_bottom_up import MendeleyBottomUpDataset
 from .pldu_dataset import PLDUDataset
 from .pldm_dataset import PLDMDataset
+from .mooring_dataset import MooringDataset
 
-__all__ = ['MendeleyPLDataset', 'MendeleyBottomUpDataset', 'PLDUDataset', 'PLDMDataset']
+__all__ = ['MendeleyPLDataset', 'MendeleyBottomUpDataset', 'PLDUDataset', 'PLDMDataset', 'MooringDataset']
 
